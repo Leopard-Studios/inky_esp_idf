b105af219147680cfc884fc4a556dad15a9efe5a
Jay <info@leopard-studios.co.uk>
2025-08-11 13:12:41 +0100

    update converter and added reqs

--- tools/image_converter.py
+++ tools/image_converter.py
@@ -7,25 +7,25 @@ from PIL import Image
 import pyarrow as pa
 
 DESATURATED_PALETTE = [
-    [0, 0, 0],
-    [255, 255, 255],
-    [0, 255, 0],
-    [0, 0, 255],
-    [255, 0, 0],
-    [255, 255, 0],
-    [255, 140, 0],
-    [255, 255, 255]
+    [0, 0, 0], #black
+    [255, 255, 255], #white
+    [0, 255, 0], #GREEN,
+    [0, 0, 255], #BLUE,
+    [255, 0, 0], #RED,
+    [255, 255, 0], #YELLOW,
+    [255, 140, 0], #ORANGE,
+    # [255, 255, 255]
 ]
 
 SATURATED_PALETTE = [
-    [57, 48, 57],
+    [0, 0, 0], #[57, 48, 57],
     [255, 255, 255],
     [58, 91, 70],
     [61, 59, 94],
     [156, 72, 75],
     [208, 190, 71],
     [177, 106, 73],
-    [255, 255, 255]
+    # [255, 255, 255]
 ]
 
 def _palette_blend(saturation, dtype="uint8"):
@@ -38,10 +38,10 @@ def _palette_blend(saturation, dtype="uint8"):
             palette += [int(rs + rd), int(gs + gd), int(bs + bd)]
         if dtype == "uint24":
             palette += [(int(rs + rd) << 16) | (int(gs + gd) << 8) | int(bs + bd)]
-    if dtype == "uint8":
-        palette += [255, 255, 255]
-    if dtype == "uint24":
-        palette += [0xFFFFFF]
+    # if dtype == "uint8":
+    #     palette += [255, 255, 255]
+    # if dtype == "uint24":
+    #     palette += [0xFFFFFF]
     return palette
 
 def parseargs():
@@ -65,7 +65,7 @@ def main():
     # Image size doesn't matter since it's just the palette we're using
     palette_image = Image.new("P", (1, 1))
     # Set our 7 colour palette (+ clear) and zero out the other 247 colours
-    palette_image.putpalette(palette + [0, 0, 0] * 248)
+    palette_image.putpalette(palette + [0, 0, 0] * 249)
     # Force source image data to be loaded for `.im` to work
     im.load()
     
